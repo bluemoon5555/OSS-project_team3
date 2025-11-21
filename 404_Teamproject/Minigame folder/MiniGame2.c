c3f290b4efdbc10223a1c5a1ac092d0c2503b2d4
YejunMoon <myj6873@gmail.com>
2025-11-21 17:42:04 +0900

    MODIFY : Modify second minigame

--- 404_Teamproject/Minigame folder/MiniGame2.c
+++ 404_Teamproject/Minigame folder/MiniGame2.c
@@ -4,52 +4,66 @@
 #include <malloc.h>
 #include <Windows.h>
 
+void SetConsoleView()
+{
+    system("mode con:cols=100 lines=25");
+    system("title Sequence Memory Game");
+}
+
+
 int main() {
-    int n;
+
+    SetConsoleView();
     int* pNums;
-    int num;
-    int ok;
-    int i;
 
-    srand((unsigned)time(NULL));
+    while (1) {
+        int n;
+        int num;
+        int ok;
+        int i;
 
-    // 숫자 개수 입력
-    printf("몇 개의 숫자 : ");
-    scanf_s("%d", &n);
+        srand((unsigned)time(NULL));
 
-    // 메모리 할당
-    pNums = (int*)malloc(sizeof(int) * n);
- 
-    // 1~100 난수들을 입력받음
-    for (i = 0; i < n; ++i)
-        pNums[i] = rand() % 100 + 1;
+        // 숫자 개수 입력
+        printf("몇 개의 숫자 : ");
+        scanf_s("%d", &n);
 
-    // 숫자들을 보여 준다
-    for (i = 0; i < n; ++i)
-    {
-        printf("\r%d", pNums[i]);
-        Sleep(1000);
-        printf("\r     ");
+        // 메모리 할당
+        pNums = (int*)malloc(sizeof(int) * n);
+
+        // 1~100 난수들을 입력받음
+        for (i = 0; i < n; ++i)
+            pNums[i] = rand() % 100 + 1;
+
+        // 숫자들을 보여 준다
+        for (i = 0; i < n; ++i)
+        {
+            printf("\r%d", pNums[i]);
+            Sleep(1000);
+            printf("\r     ");
+        }
+
+        printf("\n\n");
+
+        // 숫자 확인
+        printf("숫자들을 순서대로 입력\n");
+
+        ok = 1;
+        for (i = 0; i < n; ++i)
+        {
+            scanf_s("%d", &num);
+            if (num != pNums[i])
+                ok = 0;
+        }
+
+        if (!ok)
+            printf("틀렸습니다.\n");
+        else {
+            printf("맞았습니다.\n");
+            break;
+        }
+
     }
-
-    printf("\n\n");
-
-    // 숫자 확인
-    printf("숫자들을 순서대로 입력\n");
-
-    ok = 1;
-    for (i = 0; i < n; ++i)
-    {
-        scanf_s("%d", &num);
-        if (num != pNums[i])
-            ok = 0;
-    }
-
-    if (!ok)
-        printf("틀렸습니다.\n");
-    else
-        printf("맞았습니다.\n");
-
     // 메모리 해제
     free(pNums);
 
