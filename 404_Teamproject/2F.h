f85d50316c309fdddb783763f37cae5a18c8915b
YejunMoon <myj6873@gmail.com>
2025-11-30 20:27:39 +0900

    Create 2F.h

---
+++ 404_Teamproject/2F.h
@@ -0,0 +1,15 @@
+#pragma once
+#define FUNCTIONS_H
+
+void slowPrintChar(const char* text, int delay);
+
+void scene(const char* text);
+
+void fileprint(char* f);
+
+void choice2F();
+
+void leftroom2F();
+
+void rightroom2F();
+
