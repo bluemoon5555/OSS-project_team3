faae643a589c273c4ef024d57a913584955a8a87
YejunMoon <myj6873@gmail.com>
2025-11-30 20:03:59 +0900

    Create game.h

---
+++ 404_Teamproject/game.h
@@ -0,0 +1,30 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void SetConsoleView();
+
+
+// 커서의 위치를 x, y로 이동하는 함수
+void GotoXY(int x, int y);
+
+
+// 키보드의 입력을 받고, 입력된 키의 값을 반환하는 함수
+int GetKeyDown();
+
+// 사람을 그리는 함수
+void DrawHuman(int humanY);
+
+
+// 나무를 그리는 함수
+void DrawTree(int treeX);
+
+// 충돌 했을때 게임오버
+void DrawGameOver(const int score);
+
+
+// 충돌했으면 1(아니면 0(반환
+int isCollision(const int treeX, const int humanY);
+
+void startGame();
+
+#endif
