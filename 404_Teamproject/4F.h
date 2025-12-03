cfe93b86092e6b5987bb95681e0eab353b8d31e3
YejunMoon <myj6873@gmail.com>
2025-12-03 16:04:12 +0900

    Create 4F.h

---
+++ 404_Teamproject/4F.h
@@ -0,0 +1,21 @@
+#pragma once
+#define FUNCTIONS_H
+
+void room3();
+void room3_plus();
+
+void addItem(const char* item);
+
+int hasItem(const char* item);
+
+void showInventory();
+
+void scene(const char* text);
+
+int room1();
+
+void room3();
+
+void room3_plus();
+
+void corridor();
