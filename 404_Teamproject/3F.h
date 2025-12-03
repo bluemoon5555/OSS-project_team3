e7399a5cf3c3fee2684f252aa4385ee71858e170
YejunMoon <myj6873@gmail.com>
2025-12-03 15:51:21 +0900

    Create 3F.h

---
+++ 404_Teamproject/3F.h
@@ -0,0 +1,39 @@
+#pragma once
+#define FUNCTIONS_H
+
+void room3();
+void room3_plus();
+void floor3();
+void floor3_left();
+void floor3_right();
+void floor2();
+
+void addItem(const char* item);
+
+int hasItem(const char* item);
+
+void showInventory();
+
+void slowPrintChar(const char* text, int delay);
+
+void scene(const char* text);
+
+void showAsciiArtFull(const char* filename);
+
+void room1();
+
+void room3();
+
+void room3_plus();
+
+void corridor();
+
+void floor3();
+
+void floor3_left();
+
+void showDiaryStory();
+
+void floor3_right();
+
+void floor2();
