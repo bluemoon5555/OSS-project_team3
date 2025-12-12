6b97054d062cb3f5470c5f4e16015052e7e67cec
YejunMoon <myj6873@gmail.com>
2025-12-12 21:01:54 +0900

    Create story.h

---
+++ 404_Teamproject/story.h
@@ -0,0 +1,20 @@
+#pragma once
+#define FUNCTIONS_H
+
+void saveGame();
+
+int loadSaveData();
+
+void printMenu();
+
+void startGame();
+
+void loadGame();
+
+void menu();
+
+void printMenu();
+
+void startStory();
+
+void loadGame();
